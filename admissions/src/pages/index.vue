c9165a6ac7e7b397f03cb354c79b8a85f59fdbc2
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2024-12-31 22:40:19 +0300

    feat: redesign admissions page layout with logo and login button

--- admissions/src/pages/index.vue
+++ admissions/src/pages/index.vue
@@ -1,23 +1,41 @@
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">
-      {{ country.name }}
-    </li>
-  </ul>
+  <v-container
+    class="text-center fill-height d-flex align-center justify-center"
+  >
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
+        <v-img
+          src="@/assets/L2SH-logo.png"
+          alt="L2SH.Admissions Logo"
+          class="responsive-logo"
+        />
+      </v-col>
+      <v-col cols="12">
+        <h1>Лицей "Вторая школа"</h1>
+        <h2>Приемная комиссия</h2>
+      </v-col>
+      <v-col cols="12" class="d-flex justify-center">
+        <v-btn color="primary" class="mx-auto sized-button" to="/login">
+          Войти
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts" setup>
-import { ref, onMounted } from "vue";
-import { supabase } from "@/lib/supabaseClient";
+<script lang="ts">
+//
+</script>
 
-const countries = ref([]);
-
-async function getCountries() {
-  const { data } = await supabase.from("countries").select();
-  countries.value = data;
+<style scoped>
+.responsive-logo {
+  max-width: 150px;
+  width: 100%;
+  height: auto;
 }
 
-onMounted(() => {
-  getCountries();
-});
-</script>
+.sized-button {
+  max-width: 200px;
+  width: 100%;
+}
+</style>
