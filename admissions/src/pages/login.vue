4087ef1f2cc93591fed19848e0ada4c4cada2d5f
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2024-12-31 22:40:25 +0300

    feat: add login and registration pages with form validation

---
+++ admissions/src/pages/login.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <v-card class="pa-5" max-width="500" width="100%" :loading="loading">
+      <v-card-title>
+        <v-btn icon color="grey" to="/" variant="text">
+          <v-icon>mdi-arrow-left</v-icon>
+          Назад
+        </v-btn>
+        <h1 class="text-center flex-grow-1">Вход</h1>
+      </v-card-title>
+      <v-form ref="form" @submit.prevent="">
+        <v-text-field
+          v-model="email"
+          label="Email"
+          :rules="[rules.required, rules.email]"
+        />
+        <v-text-field
+          v-model="password"
+          label="Пароль"
+          :type="passwordVisible ? 'text' : 'password'"
+          :rules="[rules.required]"
+        >
+          <template #append-inner>
+            <v-icon @click="passwordVisible = !passwordVisible">
+              {{ passwordVisible ? "mdi-eye-off" : "mdi-eye" }}
+            </v-icon>
+          </template>
+        </v-text-field>
+        <v-btn
+          color="primary"
+          type="submit"
+          class="mt-4 mx-auto d-block"
+          :disabled="loading"
+          @click="login"
+        >
+          Войти
+        </v-btn>
+      </v-form>
+      <v-card-actions>
+        <v-btn
+          color="secondary"
+          to="/register"
+          class="mt-4 mx-auto"
+          variant="text"
+        >
+          Нет аккаунта? Зарегистрироваться
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+  <v-snackbar v-model="errorSnackbar" :timeout="5000" color="error">
+    {{ errorText }}
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import { supabase } from "@/lib/supabaseClient";
+import { defineComponent } from "vue";
+import type { VForm } from "vuetify/components";
+
+export default defineComponent({
+  data() {
+    return {
+      email: "",
+      password: "",
+      passwordVisible: false,
+      rules: {
+        required: (v: string) => !!v || "Обязательное поле",
+        email: (v: string) =>
+          /.+@.+\..+/.test(v) || "Email должен быть действительным",
+      },
+      errorSnackbar: false,
+      errorText: "",
+      loading: false,
+    };
+  },
+  methods: {
+    async login() {
+      const isValid = await (this.$refs.form as VForm).validate();
+      if (!isValid.valid) return;
+
+      this.loading = true;
+
+      const { error } = await supabase.auth.signInWithPassword({
+        email: this.email,
+        password: this.password,
+      });
+
+      this.loading = false;
+
+      if (error) {
+        this.errorText = error.message;
+        this.errorSnackbar = true;
+      } else {
+        this.$router.push("/account");
+      }
+    },
+  },
+});
+</script>
+
+<style scoped>
+.fill-height {
+  height: 100vh;
+}
+</style>
---
+++ admissions/src/pages/register.vue
@@ -0,0 +1,147 @@
+<template>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <v-card class="pa-5" max-width="500" width="100%" :loading="loading">
+      <v-card-title>
+        <v-btn icon color="grey" to="/" variant="text">
+          <v-icon>mdi-arrow-left</v-icon>
+          Назад
+        </v-btn>
+        <h1 class="text-center flex-grow-1">Регистрация</h1>
+      </v-card-title>
+      <v-form ref="form" @submit.prevent="">
+        <v-text-field
+          v-model="email"
+          label="Email"
+          type="email"
+          :rules="[rules.required, rules.email]"
+        />
+        <v-text-field
+          v-model="password"
+          label="Пароль"
+          :type="passwordVisible ? 'text' : 'password'"
+          :rules="[
+            rules.required,
+            rules.passwordLength,
+            rules.passwordUppercase,
+            rules.passwordLowercase,
+            rules.passwordNumber,
+            rules.passwordSpecial,
+            rules.passwordNoCyrillic,
+          ]"
+        >
+          <template #append-inner>
+            <v-icon @click="passwordVisible = !passwordVisible">
+              {{ passwordVisible ? "mdi-eye-off" : "mdi-eye" }}
+            </v-icon>
+          </template>
+        </v-text-field>
+        <v-text-field
+          v-model="passwordConfirmation"
+          label="Подтвердите пароль"
+          type="password"
+          :rules="[rules.required, rules.passwordMatch]"
+        />
+        <v-btn
+          color="primary"
+          type="submit"
+          class="mt-4 mx-auto d-block"
+          :disabled="loading"
+          @click="signup"
+        >
+          Зарегистрироваться
+        </v-btn>
+      </v-form>
+      <v-card-actions>
+        <v-btn
+          color="secondary"
+          to="/login"
+          class="mt-4 mx-auto"
+          variant="text"
+        >
+          Уже есть аккаунт? Войти
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+  <v-snackbar v-model="errorSnackbar" :timeout="5000" color="error">
+    {{ errorText }}
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { VForm } from "vuetify/components";
+import { supabase } from "@/lib/supabaseClient";
+
+export default defineComponent({
+  data() {
+    return {
+      email: "",
+      password: "",
+      passwordVisible: false,
+      passwordConfirmation: "",
+      rules: {
+        required: (v: string) => !!v || "Обязательное поле",
+        email: (v: string) =>
+          /.+@.+\..+/.test(v) || "Email должен быть действительным",
+        passwordLength: (v: string) =>
+          v.length >= 12 || "Пароль должен содержать не менее 12 символов",
+        passwordUppercase: (v: string) =>
+          /[A-Z]/.test(v) ||
+          "Пароль должен содержать хотя бы одну заглавную букву",
+        passwordLowercase: (v: string) =>
+          /[a-z]/.test(v) ||
+          "Пароль должен содержать хотя бы одну строчную букву",
+        passwordNumber: (v: string) =>
+          /[0-9]/.test(v) || "Пароль должен содержать хотя бы одну цифру",
+        passwordSpecial: (v: string) =>
+          /[-_|<>~!@#$%^&*(){}]/.test(v) ||
+          "Пароль должен содержать хотя бы один спецсимвол",
+        passwordNoCyrillic: (v: string) =>
+          !/[а-яА-ЯёЁ]/.test(v) ||
+          "Пароль не должен содержать кириллические буквы",
+        passwordMatch: () =>
+          this.passwordConfirmation === this.password || "Пароли не совпадают",
+      },
+      errorSnackbar: false,
+      errorText: "",
+      loading: false,
+    };
+  },
+  methods: {
+    async signup() {
+      const isValid = await (this.$refs.form as VForm).validate();
+      if (!isValid.valid) return;
+
+      this.loading = true;
+
+      const frontendUrl = import.meta.env.VITE_FRONTEND_URL ?? "";
+      if (!frontendUrl)
+        throw new Error("VITE_FRONTEND_URL environment variable is not set");
+
+      const { error } = await supabase.auth.signUp({
+        email: this.email,
+        password: this.password,
+        options: {
+          emailRedirectTo: frontendUrl + "/account",
+        },
+      });
+
+      this.loading = false;
+
+      if (error) {
+        this.errorText = error.message;
+        this.errorSnackbar = true;
+      } else {
+        this.$router.push("/account");
+      }
+    },
+  },
+});
+</script>
+
+<style scoped>
+.fill-height {
+  height: 100vh;
+}
+</style>
