33c63b1d116d5dfafabd74f141f6ba7cd6beeedb
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-01-09 02:03:01 +0300

    feat: remove Docker configuration files and add deployment script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -e
+
+yarn build
+
+cd dist
+
+cp index.html 404.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:L2SH-Dev/admissions.git main:deploy
+
+cd -
