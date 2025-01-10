97d3121e1a173126b8a0dd1a134c1e9b03452915
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-01-11 00:06:12 +0300

    feat: add authed layout with navigation drawer and app bar

---
+++ src/layouts/authed.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list>
+        <v-list-item to="/profiles" prepend-icon="mdi-account-multiple">
+          Профили
+        </v-list-item>
+        <v-list-item to="/chat" prepend-icon="mdi-chat">Поддержка</v-list-item>
+        <v-list-item to="/exams" prepend-icon="mdi-file-document">
+          Экзамены
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar style="position: fixed" :color="appBarColor" elevation="3">
+      <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer" />
+      <div class="d-flex align-center">
+        <v-img
+          src="@/assets/L2SH-logo.png"
+          width="48"
+          height="48"
+          class="me-2 ml-4"
+          alt="L2SH Logo"
+          @click="goToIndex"
+        />
+        <div class="d-none d-md-flex">
+          <v-btn to="/exams" prepend-icon="mdi-file-document" class="mx-2">
+            Экзамены
+          </v-btn>
+          <v-btn to="/chat" prepend-icon="mdi-chat" class="mx-2">
+            Поддержка
+          </v-btn>
+        </div>
+      </div>
+      <v-spacer />
+      <div class="d-none d-md-flex">
+        <v-btn
+          to="/profiles"
+          prepend-icon="mdi-account-multiple"
+          class="ms-2 mr-4"
+        >
+          Профили
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { ref, computed } from "vue";
+import { useTheme } from "vuetify";
+
+const router = useRouter();
+const drawer = ref(false);
+const theme = useTheme();
+
+const goToIndex = () => {
+  router.push("/");
+};
+
+const appBarColor = computed(() => {
+  return theme.global.current.value.dark ? "" : "#EEEEEE";
+});
+</script>
