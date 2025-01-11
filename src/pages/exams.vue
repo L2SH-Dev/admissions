12b559836ab44cefb158319215d61cbfb815e03c
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-01-12 00:14:26 +0300

    feat: add profile selection prompt to exams page when no profile is selected

--- src/pages/exams.vue
+++ src/pages/exams.vue
@@ -1,6 +1,19 @@
 <template>
-  <h1>EXAMS</h1>
+  <v-empty-state
+    v-if="store.profile === null"
+    icon="mdi-book-open-variant"
+    text="Выберите профиль для просмотра экзаменов"
+    title="Не выбран профиль"
+    action-text="Выбрать профиль"
+    @click:action="() => $router.push('/profiles')"
+  />
 </template>
+
+<script lang="ts" setup>
+import { useAppStore } from "@/stores/app";
+
+const store = useAppStore();
+</script>
 
 <route lang="yaml">
 meta:
