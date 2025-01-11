8f01721d5711812b5764f06e65b6d48dd0bf7953
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-01-12 00:14:15 +0300

    feat: add pinia-plugin-persistedstate for state persistence and enhance app store with profile management actions

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,38 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useAppStore = defineStore('app', {
+export const useAppStore = defineStore("app", {
+  persist: true,
   state: () => ({
-    //
+    profile: null as Profile | null,
   }),
-})
+  actions: {
+    setProfile(profile: Profile) {
+      this.profile = profile;
+    },
+    clearProfileID() {
+      this.profile = null;
+    },
+  },
+});
+
+export interface Profile {
+  id: string;
+  approved: boolean;
+  birth_date: string;
+  created_at: string;
+  first_name: string;
+  last_name: string;
+  patronymic: string | null;
+  gender: string | null;
+  grade: number;
+  old_school: string;
+  parent_first_name: string;
+  parent_last_name: string;
+  parent_patronymic: string | null;
+  parent_phone: string;
+  source: string;
+  june_exam: boolean;
+  vmsh: boolean;
+  avatar: string;
+}
