edc084d929b0837010ddf16e3b9716cbcd2285a9
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-01-17 22:50:04 +0300

    feat: add custom 404 page and update router types

---
+++ src/pages/[...404].vue
@@ -0,0 +1,9 @@
+<template>
+  <v-empty-state
+    headline="404"
+    :text="$route.fullPath"
+    title="Страница не найдена"
+    action-text="На главную"
+    @click:action="() => $router.push('/')"
+  />
+</template>
