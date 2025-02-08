8daeb9b471025852671ccd81d002f755ac5a1c5a
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-02-08 04:46:40 +0300

    feat: rewrite get_allocation function as plsql script

--- database-functions/get_allocation.sql
+++ database-functions/get_allocation.sql
@@ -1,7 +1,7 @@
--- schema: public
--- return type: integer
--- arguments: [ (e_id int4) ]
--- language: sql
--- configuration parameters: [ (search_path '') ]
--- security: definer
-select count(*) from public.exam_registrations where exam_id = e_id;
+create or replace function get_allocation(e_id bigint)
+returns integer
+language sql
+security definer set search_path = ''
+as $$
+    select count(*) from public.exam_registrations where exam_id = e_id;
+$$;
